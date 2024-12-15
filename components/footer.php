664892ebc00238ab1812f607c0d13f110367e9f5
mint. <erwan.graveline@gmail.com>
2024-12-15 12:04:39 +0100

    Add files via upload

--- components/footer.php
+++ components/footer.php
@@ -1,1 +1,10 @@
-
+<footer>
+    <link rel="stylesheet" type="text/css" href="src/styles/footer.css">
+    <div class="container">
+        <div class="copyrights">
+            <p>Planori &nbsp;</p>
+            <p>© &nbsp; 2024</p>
+            <p>&nbsp; Nolimix</p>
+        </div>
+    </div>
+</footer>
